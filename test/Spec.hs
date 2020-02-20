7553e851444de4172c382fdb308e5195234a91ed
Vicente Reyes <vreyesvaldivieso@gmail.com>
2020-02-21 00:09:48 +0900

    make tests decent

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,12 @@
 import Test.HUnit
-import Data.Set as Set
+import qualified Data.Set as Set
 import DS
 
-main = runTestTT $ TestList [test1, test2, test3]
+main = runTestTT $ TestList $ map TestCase
+  [ 1 @=? mass mempty numbers numbers
+  , alice @=? alice <> mempty
+  , aliceAndBob @=? mempty <> aliceAndBob
+  , aliceAndBob' @=? aliceAndBob ]
 
 numbers = Set.fromList [1..5]
 numbers' = fromMasses numbers
@@ -11,7 +15,3 @@ alice = numbers' [([1], 0.5), ([2], 0.5)]
 bob = numbers' [([2], 0.2), ([3], 0.8)]
 aliceAndBob = alice <> bob
 aliceAndBob' = numbers' [([2], 1)]
-
-test1 = TestCase (assertEqual "mempty" 1 (mass mempty numbers numbers))
-test2 = TestCase (assertEqual "<>mempty" alice (alice<>mempty))
-test3 = TestCase (assertEqual "alice and bob" aliceAndBob' aliceAndBob)
